bb2d1d585be847e323e111bd8b79dda3e5a0e499
Nikhil Kumar <nikhilkrkanti@gmail.com>
2025-05-26 12:53:47 +0530

    Last commit was wrong it should be same as but there was issue with .env only where instead of VITE_RAZORPAY_KEY_ID i erongly typed RAZORPAY_KEY_ID

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -43,7 +43,7 @@ const PlaceOrder = () => {
 
     const options = {
       
-      key : import.meta.RAZORPAY_KEY_ID,
+      key : import.meta.VITE_RAZORPAY_KEY_ID,
       amount : order.amount,
       currency: order.currency,
       name : 'Order Payment',
